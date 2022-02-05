70f4b7bb8b3f9c7434ce5dca72229cc2a31b1f35
bhowland <benjhowland@gmail.com>
2022-02-05 00:04:00 -0600

    Changed to RecyclerView

--- app/src/main/java/com/example/notekeeper/NoteActivity.kt
+++ app/src/main/java/com/example/notekeeper/NoteActivity.kt
@@ -9,7 +9,7 @@ import android.widget.ArrayAdapter
 import com.example.notekeeper.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.content_main.*
 
-class MainActivity : AppCompatActivity() {
+class NoteActivity : AppCompatActivity() {
     private var notePosition = POSITION_NOT_SET
 
     private lateinit var appBarConfiguration: AppBarConfiguration
